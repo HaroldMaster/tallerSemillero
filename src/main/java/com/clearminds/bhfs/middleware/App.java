60df4cfe9b11118c1abf7a7fb5164df268a35c9a
Harold Flores <bhflores1994@gmail.com>
2020-12-09 09:21:02 -0500

    Versión inicial

---
+++ src/main/java/com/clearminds/bhfs/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.bhfs.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
