6c391225880029ee1a95156f586754ffa941ca08
Harold Flores <bhflores1994@gmail.com>
2020-12-09 11:44:48 -0500

    Se modifica insertarEstudiante para que inserte en la base de datos

--- src/main/java/com/clearminds/bhfs/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/bhfs/servicios/ServicioEstudiante.java
@@ -1,5 +1,8 @@
 package com.clearminds.bhfs.servicios;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.clearminds.bhfs.dtos.Estudiante;
 import com.clearminds.bhfs.excepciones.BDDException;
 
@@ -19,6 +22,18 @@ public class ServicioEstudiante extends ServicioBase {
 	public void insertarEstudiante(Estudiante estudiante) throws BDDException{
 		abrirConexion();
 		System.out.println("Insertando Estudiante: " +estudiante);
-		cerrarConexion();
+		Statement stmt = null;
+		try {
+			stmt = getConexion().createStatement();
+			String sql = "insert into estudiantes(nombre,apellido) values('"+estudiante.getNombre()+"','"+estudiante.getApellido()+"')";
+			System.out.println("Script: "+sql);
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new BDDException("Error al insertar estudiante");
+		}
+		
 	}
 }
